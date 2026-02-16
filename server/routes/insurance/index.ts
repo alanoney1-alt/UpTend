bb710d07809186396de136d799c2ccbdb58de1ab
Alan Oney <ao@Mac-mini.local>
2026-02-16 14:52:52 -0500

    Fix ESM import paths for insurance routes

--- server/routes/insurance/index.ts
+++ server/routes/insurance/index.ts
@@ -1,6 +1,6 @@
 import type { Express } from "express";
-import { registerInsurancePolicyRoutes } from "./policies.routes";
-import { registerClaimsRoutes } from "./claims.routes";
+import { registerInsurancePolicyRoutes } from "./policies.routes.js";
+import { registerClaimsRoutes } from "./claims.routes.js";
 
 export function registerInsuranceRoutes(app: Express) {
   registerInsurancePolicyRoutes(app);
